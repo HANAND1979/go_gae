77a9dca5710e2ab8d90e4e8fd3293a2b7975f5b7
Anand Hariharan <HANAND1979@users.noreply.github.com>
2016-03-09 07:00:28 +0530

    V.1.0
    Uploaded first version of files

---
+++ myapp/simple_hello/hello.go
@@ -0,0 +1,15 @@
+// myapp
+package myapp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<b>Hello World & Anand!</b>")
+}
