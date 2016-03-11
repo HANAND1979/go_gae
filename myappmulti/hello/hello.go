a3578c500e44af5ae137b7ef055f2f7307fd598a
Anand Hariharan <HANAND1979@users.noreply.github.com>
2016-03-11 09:40:27 +0530

    Added new example on multiple modules

---
+++ myappmulti/hello/hello.go
@@ -0,0 +1,15 @@
+// hello
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<b>Hello !</b>")
+}
